fb269eed83b859ae598fb37d4c4f8a6aee7d6f3a
Japar12 <abduljapar84@gmail.com>
2024-06-25 20:45:26 +0700

    first commit

--- app/routes/order.route.js
+++ app/routes/order.route.js
@@ -1,10 +1,23 @@
+// module.exports = (app) => {
+//   const order = require("../controllers/order.controller");
+
+//   let router = require("express").Router();
+
+//   // look a cart
+//   router.get("/user/:id", order.findCart);
+//   router.post("/update/user/:id", order.addToCart);
+//   router.delete("/delete/user/:id/product/:code", order.removeFromCart);
+
+//   app.use("/api/orders", router);
+// };
+
 module.exports = (app) => {
   const orders = require("../controllers/order.controller");
   const router = require("express").Router();
 
   router.get("/user/:id", orders.findOrder);
-  router.post("/user/:id/add", orders.addToCart);
-  router.delete("/user/:id/product/:product", orders.removeFromCart);
+  router.post("/update/user/:id/", orders.addToCart);
+  router.delete("/delete/user/:id/product/:code", orders.removeFromCart);
 
   app.use("/api/orders", router);
 };
